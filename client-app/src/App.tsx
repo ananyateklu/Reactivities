7cac304c222e0a606f687aaaaca6622975961868
Ananya Teklu <teklu009@morris.umn.edu>
2021-04-27 16:29:50 -0500

    Finalize walking skeleton of the app

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,32 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { ducks } from './demo';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {ducks.map(duck => (
-          <div key={duck.name}>
-            <span>{duck.name}</span>
-            <button onClick={() => duck.makeSound(duck.name + ' quack')}>make sound</button>
+  const [activities, setActivities] = useState([])
 
-          </div>
-        ))}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      console.log(response);
+      setActivities(response.data);
+    })
+  }, [])
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+        <List>
+        {activities.map((activity: any) => (
+            <List.Item key={activity.id}>
+               {activity.title}
+            </List.Item> 
+             
+          ))}
+       
+        </List>
+      
     </div>
   );
 }
