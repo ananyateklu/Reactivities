d0ddf4bd871ff315f98e66aabe5639b58ddb6441
Ananya Teklu <teklu009@morris.umn.edu>
2021-04-29 18:21:58 -0500

    Implement button click to open form and card

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -8,18 +8,29 @@ import ActivityList from './ActivityList';
 
 interface Props {
     activities: Activity[];
+    selectedActivity: Activity | undefined;
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    editMode: boolean;
+    openForm: (id: string) => void;
+    closeForm: () => void;
 }
 
-export default function ActivityDashboard(props: Props) {
+export default function ActivityDashboard({activities, selectedActivity,
+     selectActivity, cancelSelectActivity, editMode, openForm, closeForm}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-             <ActivityList activities={props.activities}/>
+             <ActivityList activities={activities} selectActivity={selectActivity}/>
             </Grid.Column>
             <Grid.Column width='6'>
-                {props.activities[0] && 
-                <ActivityDetails activity={props.activities[0]}/>}
-                <ActivityForm/>
+                {selectedActivity && !editMode &&
+                <ActivityDetails activity={selectedActivity} 
+                cancelSelectActivity={cancelSelectActivity}
+                openForm={openForm}
+                />}
+                {editMode &&
+                <ActivityForm closeForm={closeForm} activity={selectedActivity}/>}
             </Grid.Column>
         </Grid>
     )
