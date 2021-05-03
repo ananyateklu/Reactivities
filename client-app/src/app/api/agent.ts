6f676fb9992d96783e21e877e52a5d003a081819
Ananya Teklu <teklu009@morris.umn.edu>
2021-05-03 16:54:50 -0500

    Adding types in axios requests This enables intellisense and requests will have types now

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,19 +1,20 @@
 import axios, { AxiosResponse } from 'axios';
+import { Activity } from '../models/activity';
 
 axios.defaults.baseURL= 'http://localhost:5000/api';
 
-const responseBody = (response: AxiosResponse) => response.data;
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
-    get: (url: string) => axios.get(url).then(responseBody),
-    post: (url: string, body: {}) => axios.get(url).then(responseBody),
-    put: (url: string, body: {}) => axios.get(url).then(responseBody),
-    del: (url: string) => axios.get(url).then(responseBody),
+    get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+    post: <T>(url: string, body: {}) => axios.post<T>(url).then(responseBody),
+    put: <T>(url: string, body: {}) => axios.put<T>(url).then(responseBody),
+    del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 
 }
 
 const Activities = {
-    list: () => requests.get('/activities')
+    list: () => requests.get<Activity[]>('/activities')
 }
 
 const agent = {
