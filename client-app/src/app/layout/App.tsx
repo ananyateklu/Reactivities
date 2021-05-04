afd16abcc39c1c8ef16d0409440921e12c460e85
Ananya Teklu <teklu009@morris.umn.edu>
2021-05-03 21:08:56 -0500

    handleCreateOrEditActivity is working now It submits requests via agent

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -12,6 +12,7 @@ function App() {
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitiing] = useState(false);
 
   useEffect(() => {
     agent.Activities.list().then(response => {
@@ -44,6 +45,26 @@ function App() {
   }
 
   function handleCreateOrEditActivity(activity: Activity) {
+    setSubmitiing(true);
+    if (activity.id) {
+      agent.Activities.update(activity).then(() => {
+        setActivities([...activities.filter(x => x.id !== activity.id), activity])
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitiing(false);
+      })
+    } else {
+      activity.id = uuid();
+      agent.Activities.create(activity).then(() => {
+        setActivities([...activities, activity])
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitiing(false);
+      })
+    }
+
+
+
     activity.id ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
       : setActivities([...activities, { ...activity, id: uuid() }]);
     setEditMode(false);
@@ -69,6 +90,7 @@ function App() {
           closeForm={handleFormClose}
           createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
+          submitting={submitting}
         />
       </Container>
     </>
