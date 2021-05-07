e7cab60ff32686707389b1da0ce755e58e96f8a5
Ananya Teklu <teklu009@morris.umn.edu>
2021-05-07 15:34:31 -0500

    Strict mode warning fix

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,4 +1,4 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
 
@@ -15,18 +15,29 @@ export default class ActivityStore {
     }
 
     loadActivities = async () => {
-        this.loadingInitial = true;
+        this.setLoadingInitial(true);
         try {
             const activities = await agent.Activities.list();
-            activities.forEach(activity => {
-                activity.date = activity.date.split('T')[0];
-                this.activities.push(activity);
-              })
-              this.loadingInitial = false;
+            runInAction(() => {
+                activities.forEach(activity => {
+                    activity.date = activity.date.split('T')[0];
+                    this.activities.push(activity);
+                  })
+                  this.setLoadingInitial(false);
+            })
+            
 
         } catch (error) {
             console.log(error);
+            runInAction(() => {
+                this.setLoadingInitial(false);
+            })
+            
         }
+    }
+
+    setLoadingInitial = (state: boolean) => {
+        this.loadingInitial = state;
     }
 
    
