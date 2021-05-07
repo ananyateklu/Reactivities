152927111b375af5c4bd32198d57c32d499d9016
Ananya Teklu <teklu009@morris.umn.edu>
2021-05-07 09:13:14 -0500

    Create Sample mobx state to try how it works

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,13 @@
+import { makeObservable, observable } from "mobx";
+
+export default class ActivityStore {
+    title = "hello from mobx";
+
+
+    constructor() {
+        makeObservable(this, {
+            title: observable
+        } )
+    }
+
+}
