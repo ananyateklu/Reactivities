91a915b8e0a05ef52457c41cf0f71f3025a4428f
Ananya Teklu <teklu009@morris.umn.edu>
2021-05-15 11:47:48 -0500

    Replace runInAction method

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,4 +1,4 @@
-import { makeAutoObservable, runInAction } from "mobx";
+import { makeAutoObservable} from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
 
@@ -18,20 +18,18 @@ export default class ActivityStore {
         this.setLoadingInitial(true);
         try {
             const activities = await agent.Activities.list();
-            runInAction(() => {
+
                 activities.forEach(activity => {
                     activity.date = activity.date.split('T')[0];
                     this.activities.push(activity);
                   })
                   this.setLoadingInitial(false);
-            })
             
 
         } catch (error) {
             console.log(error);
-            runInAction(() => {
                 this.setLoadingInitial(false);
-            })
+     
             
         }
     }
